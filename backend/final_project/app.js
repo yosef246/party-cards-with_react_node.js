28763d3b7abc767fc43852e0a2ce155224dd305c
Your Name <you@example.com>
2025-11-11 10:43:11 +0200

    update

--- backend/final_project/app.js
+++ backend/final_project/app.js
@@ -21,6 +21,12 @@ app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin) return callback(null, true); // מאפשר Postman / curl
+
+      // ✅ מאפשר כל דומיין שמגיע מ-Vercel
+      if (origin.includes("vercel.app")) {
+        return callback(null, true);
+      }
+
       if (allowedOrigins.indexOf(origin) === -1) {
         return callback(new Error("CORS policy not allowed"), false);
       }
